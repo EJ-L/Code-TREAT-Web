2a72db85131f96939c8bc857ede92a09d30ab1ff
Derek Ng <84830145+derek33125@users.noreply.github.com>
2025-07-04 00:20:03 +0800

    Removing the compare button in the overall leaderboard

--- src/app/components/sections/Leaderboard/ResultsTable.tsx
+++ src/app/components/sections/Leaderboard/ResultsTable.tsx
@@ -113,19 +113,22 @@ const ResultsTable: FC<ResultsTableProps> = ({
             {currentTask.charAt(0).toUpperCase() + currentTask.slice(1)} Results
           </h2>
           <div className="flex space-x-2">
-            <button
-              onClick={() => setIsComparisonModalOpen(true)}
-              className={`
-                px-4 py-2 rounded-lg text-white transition-all text-base
-                bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600
-                flex items-center gap-2
-              `}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z" />
-              </svg>
-              Compare
-            </button>
+            {/* Hide compare button for overall task since there are no metrics to compare */}
+            {currentTask !== 'overall' && (
+              <button
+                onClick={() => setIsComparisonModalOpen(true)}
+                className={`
+                  px-4 py-2 rounded-lg text-white transition-all text-base
+                  bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600
+                  flex items-center gap-2
+                `}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z" />
+                </svg>
+                Compare
+              </button>
+            )}
             
             <ClientOnlyCSVLink
               data={csvData.data}
